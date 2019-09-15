0a356b92865bb2bd37961a0c8b0c42065d7e596b
txemaromero <txema.romero@gmail.com>
2019-09-15 12:36:27 +0200

    Update install-project-inside-docker-container.sh

--- install-project-inside-docker-container.sh
+++ install-project-inside-docker-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "We simply install Docker and run the container (over Raspbian/ARM) ..."
+echo "We simply install Docker and run the container ..."
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -10,10 +10,6 @@ curl -sSL https://get.docker.com | sh
 sudo groupadd docker
 sudo usermod -aG docker pi
 newgrp docker
-
-sudo apt-get install debootstrap
-sudo debootstrap --variant=minbase --arch=armhf stretch raspbian-stretch http://archive.raspbian.org/raspbian
-sudo tar -C raspbian-stretch -c . | docker import - raspbian-stretch
 
 docker build -t ethereum-internet-access .
 docker container run --name=my-ethereum-internet-access ethereum-internet-access
