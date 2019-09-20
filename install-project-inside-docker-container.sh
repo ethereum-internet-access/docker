8ee99f3cb9ffd8c0cb393964049781092e31a37f
txemaromero <txema.romero@gmail.com>
2019-09-20 20:38:22 +0200

    Update install-project-inside-docker-container.sh

--- install-project-inside-docker-container.sh
+++ install-project-inside-docker-container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-echo "We simply install Docker and run the container ..."
-
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get install curl
-curl -sSL https://get.docker.com | sh
+echo "We simply run the container that includes the project application."
 
 sudo groupadd docker
 sudo usermod -aG docker pi
